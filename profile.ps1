235b31231f299d1e4ae6655f830d90b7329a7cac
Ville Heikkinen <81528449+vilhei@users.noreply.github.com>
2023-04-11 13:49:36 +0300

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,14 +1,30 @@
 Import-Module Az.Tools.Predictor
+Import-Module PoshColor
+Import-Module posh-git
+
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 Set-PSReadLineOption -PredictionViewStyle ListView 
 
-Import-Module PoshColor
 # Set-PoshColorTheme DefaultHighColor
+
+$GitPromptSettings.DefaultPromptPrefix.Text = ""
+$GitPromptSettings.DefaultPromptSuffix.Text = ""
+$GitPromptSettings.DefaultPromptPath = " "
+
 
 # Change the prompt from showing the full path
 function prompt {
-  $p = "PS: " + ((Get-Location).drive.name) + "\...\" + (Split-Path -leaf -path (Get-Location))
-  "$p> "
+    $p = Get-Location
+    if ((Get-Location).path.split("\").count -gt 4) {
+        <# Action to perform if the condition is true #>
+        $p = ((Get-Location).drive.name) 
+        $p += ":\...\" 
+        $p += (Split-Path -path (Get-Location) -Parent) | Split-Path -leaf
+        $p += "\"
+        $p += Split-Path -path (Get-Location) -Leaf
+        $p += & $GitPromptScriptBlock
+    }
+    "$p> "
 }
 
 New-Alias -Name "codei" code-insiders
