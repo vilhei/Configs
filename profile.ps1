a5b60fd35d10809f454fb78aadb6b173f4d48025
Ville Heikkinen <81528449+vilhei@users.noreply.github.com>
2023-04-25 10:18:05 +0300

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,4 +1,9 @@
-Import-Module Az.Tools.Predictor
+New-Alias -Name "codei" code-insiders
+New-Alias -Name "gs" Get-GitStatus
+New-Alias -Name "gcom" Set-GitCommit
+
+# Import-Module Az.Tools.Predictor
+Import-Module CompletionPredictor
 Import-Module PoshColor
 Import-Module posh-git
 
@@ -11,20 +16,29 @@ $GitPromptSettings.DefaultPromptPrefix.Text = ""
 $GitPromptSettings.DefaultPromptSuffix.Text = ""
 $GitPromptSettings.DefaultPromptPath = " "
 
-
 # Change the prompt from showing the full path
 function prompt {
-    $p = Get-Location
-    if ((Get-Location).path.split("\").count -gt 4) {
-        <# Action to perform if the condition is true #>
-        $p = ((Get-Location).drive.name) 
-        $p += ":\...\" 
-        $p += (Split-Path -path (Get-Location) -Parent) | Split-Path -leaf
-        $p += "\"
-        $p += Split-Path -path (Get-Location) -Leaf
-        $p += & $GitPromptScriptBlock
-    }
-    "$p> "
+  $p = Get-Location
+  if ((Get-Location).path.split("\").count -gt 4) {
+    <# Action to perform if the condition is true #>
+    $p = ((Get-Location).drive.name) 
+    $p += ":\...\" 
+    $p += (Split-Path -path (Get-Location) -Parent) | Split-Path -leaf
+    $p += "\"
+    $p += Split-Path -path (Get-Location) -Leaf
+    $p += & $GitPromptScriptBlock
+  }
+  "$p> "
 }
 
-New-Alias -Name "codei" code-insiders
+function cwd {
+  Set-Clipboard (Get-Location)
+}
+
+function Get-GitStatus {
+  git status $args
+}
+
+function Set-GitCommit {
+  git commit $args
+}
