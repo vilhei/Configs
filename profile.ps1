a0714b2d897ddc0c085f5149b5b3b743f845bf51
Ville Heikkinen <81528449+vilhei@users.noreply.github.com>
2024-01-10 16:29:50 +0200

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,36 +1,13 @@
-New-Alias -Name "codei" code-insiders
 New-Alias -Name "gs" Get-GitStatus
 New-Alias -Name "gcom" Set-GitCommit
+New-Alias -Name "g" Set-Git
 
-# Import-Module Az.Tools.Predictor
 Import-Module CompletionPredictor
 Import-Module PoshColor
 Import-Module posh-git
 
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 Set-PSReadLineOption -PredictionViewStyle ListView 
-
-# Set-PoshColorTheme DefaultHighColor
-
-$GitPromptSettings.DefaultPromptPrefix.Text = ""
-$GitPromptSettings.DefaultPromptSuffix.Text = ""
-$GitPromptSettings.DefaultPromptPath = " "
-
-# Change the prompt from showing the full path
-function prompt {
-  $p = Get-Location
-  if ((Get-Location).path.split("\").count -gt 4) {
-    <# Action to perform if the condition is true #>
-    $p = ((Get-Location).drive.name) 
-    $p += ":\...\" 
-    $p += (Split-Path -path (Get-Location) -Parent) | Split-Path -leaf
-    $p += "\"
-    $p += Split-Path -path (Get-Location) -Leaf
-    $p += & $GitPromptScriptBlock
-  }
-  "$p> "
-  $Host.UI.RawUI.WindowTitle = (Split-Path -path $p -leaf)  
-}
 
 function cwd {
   Set-Clipboard (Get-Location)
@@ -43,3 +20,14 @@ function Get-GitStatus {
 function Set-GitCommit {
   git commit $args
 }
+
+function Set-Git {
+  git  $args
+}
+
+function time {
+  $v = (Invoke-Expression "$args")
+  Measure-Command { $v |  Out-Default }
+}
+
+oh-my-posh init pwsh --config "https://raw.githubusercontent.com/vilhei/Configs/main/ohmyposh.omp.json" | Invoke-Expression
