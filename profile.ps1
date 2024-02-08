bdb76e4f9c6e958a68e90f19a0e167f5f6a666d4
Ville Heikkinen <81528449+vilhei@users.noreply.github.com>
2024-02-08 10:05:31 +0200

    profile.ps1, auto inserts matching parentheses when typing opening one

--- profile.ps1
+++ profile.ps1
@@ -9,6 +9,9 @@ Import-Module posh-git
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 Set-PSReadLineOption -PredictionViewStyle ListView 
 
+Set-PSReadLineKeyHandler -Key Ctrl+d -Function SelectForwardWord
+Set-PSReadLineKeyHandler -Key Ctrl+d -Function SelectBackwardWord
+
 function cwd {
   Set-Clipboard (Get-Location)
 }
@@ -17,7 +20,7 @@ function Get-GitStatus {
   git status $args
 }
 
-function Set-GitCommit {
+function Set-GitCommit {  
   git commit $args
 }
 
@@ -25,11 +28,69 @@ function Set-Git {
   git  $args
 }
 
+# function time {
+#   $v = (Invoke-Expression "$args")
+#   Measure-Command { $v |  Out-Default }
+# }
+
+
 function time($block) {
-    $sw = [Diagnostics.Stopwatch]::StartNew()
-    &$block @args
-    $sw.Stop()
-    $sw.Elapsed
+  $sw = [Diagnostics.Stopwatch]::StartNew()
+  &$block @args
+  $sw.Stop()
+  $sw.Elapsed
+}
+
+
+Set-PSReadLineKeyHandler -Key '(', '{', '[' `
+  -BriefDescription InsertPairedBraces `
+  -LongDescription "Insert matching braces" `
+  -ScriptBlock {
+  param($key, $arg)
+
+  $closeChar = switch ($key.KeyChar) {
+    <#case#> '(' { [char]')'; break }
+    <#case#> '{' { [char]'}'; break }
+    <#case#> '[' { [char]']'; break }
+  }
+
+  $selectionStart = $null
+  $selectionLength = $null
+  [Microsoft.PowerShell.PSConsoleReadLine]::GetSelectionState([ref]$selectionStart, [ref]$selectionLength)
+
+  $line = $null
+  $cursor = $null
+  [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+    
+  if ($selectionStart -ne -1) {
+    # Text is selected, wrap it in brackets
+    [Microsoft.PowerShell.PSConsoleReadLine]::Replace($selectionStart, $selectionLength, $key.KeyChar + $line.SubString($selectionStart, $selectionLength) + $closeChar)
+    [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($selectionStart + $selectionLength + 2)
+  }
+  else {
+    # No text is selected, insert a pair
+    [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$($key.KeyChar)$closeChar")
+    [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
+  }
+}
+
+Set-PSReadLineKeyHandler -Key ')', ']', '}' `
+  -BriefDescription SmartCloseBraces `
+  -LongDescription "Insert closing brace or skip" `
+  -ScriptBlock {
+  param($key, $arg)
+
+  $line = $null
+  $cursor = $null
+  [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+
+  if ($line[$cursor] -eq $key.KeyChar) {
+    [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
+  }
+  else {
+    [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$($key.KeyChar)")
+  }
 }
 
 oh-my-posh init pwsh --config "https://raw.githubusercontent.com/vilhei/Configs/main/ohmyposh.omp.json" | Invoke-Expression
+
